a40a44c4bdde4e70f57b9b387298a6fe5a31506e
孙祥智 <736644851@qq.com>
2026-04-24 21:53:24 +0800

    初始化工程

---
+++ src/main/java/org/nowhatwhy/ahut/AhutApplication.java
@@ -0,0 +1,13 @@
+package org.nowhatwhy.ahut;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AhutApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AhutApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/nowhatwhy/ahut/AhutApplicationTests.java
@@ -0,0 +1,13 @@
+package org.nowhatwhy.ahut;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AhutApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
